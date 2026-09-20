e06bfccc2be5728b8e9c865f830b34417205718f
nishi <102120346+nishia1@users.noreply.github.com>
2026-09-19 20:59:17 -0400

    Force dynamic rendering for ImportPage

--- src/app/import/page.tsx
+++ src/app/import/page.tsx
@@ -1,6 +1,8 @@
 import ImportView from "@/app/import/ImportView";
 import { getPeople } from "@/lib/world";
 
+export const dynamic = "force-dynamic";
+
 export default async function ImportPage() {
   return <ImportView people={await getPeople()} />;
 }
